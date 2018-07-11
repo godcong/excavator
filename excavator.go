ea92afcd67d3598140470d12c401b7a7375e18df
godcong <jumbycc@163.com>
2018-07-11 17:20:45 +0800

    char get test

--- excavator.go
+++ excavator.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+type CharacterAssign func(c *Character, s string) bool
 
 //getRootList run get root
 func getRootList(r *Root, suffix string) {
@@ -71,9 +74,11 @@ func getRedicalList(r *Root, radical *Radical) {
 							Pinyin:         ch[0],
 							Radical:        ch[1],
 							RadicalStrokes: radical.Strokes,
+							TotalStrokes:   "",
 							KangxiStrokes:  ch[2],
 							Phonetic:       "",
 							Folk:           Folk{},
+							Structure:      Structure{},
 						})
 					}
 				}
@@ -83,6 +88,111 @@ func getRedicalList(r *Root, radical *Radical) {
 	})
 }
 
+func saveRadical(c *Character, v string) bool {
+	log.Println(v)
+	c.Radical = v
+	return true
+}
+
+func saveRadicalStrokes(c *Character, v string) bool {
+	log.Println(v)
+	c.RadicalStrokes = v
+	return true
+}
+
+func saveTotalStrokes(c *Character, v string) bool {
+	log.Println(v)
+	c.TotalStrokes = v
+	return true
+}
+
+func saveKangXiStrokes(c *Character, v string) bool {
+	log.Println(v)
+	c.KangxiStrokes = v
+	return true
+}
+
+func savePinyin(c *Character, v string) bool {
+	log.Println(v)
+	c.Pinyin = v
+	return true
+}
+
+func savePhonetic(c *Character, v string) bool {
+	log.Println(v)
+	c.Phonetic = v
+	return true
+}
+
+func dummySave(c *Character, v string) bool {
+	log.Println(v)
+	return true
+}
+
+var caMap = map[string]CharacterAssign{
+	"部首笔画：":  saveRadicalStrokes,
+	"部首：":    saveRadical,
+	"总笔画：":   saveTotalStrokes,
+	"康熙字典笔画": saveKangXiStrokes,
+	"拼音：":    savePinyin,
+	"注音：":    savePhonetic,
+}
+
+func characterAssignFunc(list map[string]CharacterAssign, text string) CharacterAssign {
+	for k, v := range list {
+		if strings.Index(text, k) >= 0 {
+			return v
+		}
+	}
+	return nil
+}
+
+func folk(c *Character, text string) bool {
+	s := strings.Split(text, "：")
+	if strings.Index(text, "是否为常用字") >= 0 {
+		if len(s) >= 2 {
+			c.Folk.CommonlyCharacters = s[1]
+		}
+
+		return true
+	} else if strings.Index(text, "姓名学") >= 0 {
+		if len(s) >= 2 {
+			c.Folk.NameScience = s[1]
+		}
+		return true
+	}
+	return false
+}
+
+func decompositionSearch(c *Character, v string) bool {
+	c.DecompositionSearch = strings.Replace(v, "]：", "", -1)
+	return true
+}
+
+func strokeNumber(c *Character, v string) bool {
+	c.StrokeNumber = strings.Replace(v, "]：", "", -1)
+	return true
+}
+func strokeReadWrite(c *Character, v string) bool {
+	c.StrokeReadWrite = strings.Replace(v, "]：", "", -1)
+	return true
+}
+
+var sMap = map[string]CharacterAssign{
+	"首尾分解查字": decompositionSearch,
+	"笔顺编号":   strokeNumber,
+	"笔顺读写":   strokeReadWrite,
+	"广　韵":    dummySave,
+	"国　语":    dummySave,
+	"粤　语":    dummySave,
+	"古文字诂林":  dummySave,
+	"故训彙纂":   dummySave,
+	"说文解字":   dummySave,
+	"康熙字典":   dummySave,
+	"汉语字典":   dummySave,
+	"辞　海":    dummySave,
+}
+
 func getCharacterList(r *Root, c *Character) {
 	url := r.URL + c.URL
 	doc, err := parseDocument(url)
@@ -90,27 +200,44 @@ func getCharacterList(r *Root, c *Character) {
 		panic(err)
 	}
 	log.Println(doc.Html())
+	docCopy := doc.Clone()
 
-	doc.Find(".pinyin").Each(func(i int, selection *goquery.Selection) {
-		text := selection.ReplaceWith("script").Text()
-		//拼音
-		if i == 0 {
-			c.Pinyin = text
-		} else
-		//注音
-		if i == 1 {
-			c.Phonetic = text
-		} else
-		//无
-		{
+	//处理笔画
+	f := dummySave
+	doc.Find("tbody tr .text15").ReplaceWith("script").Each(func(i int, selection *goquery.Selection) {
+		selection.Contents().Each(func(i int, selection *goquery.Selection) {
+			text := selection.Text()
 
-		}
+			if f != nil {
+				if b := f(c, text); b {
+					f = nil
+				}
+			}
+
+			f = characterAssignFunc(caMap, text)
+
+		})
 	})
 
-	//TODO
-	log.Println(doc.Find(".text16").Each(func(i int, selection *goquery.Selection) {
-		log.Println(selection.Html())
-	}))
+	docCopy.Find(".text16").Each(func(i int, selection *goquery.Selection) {
+		selection.Contents().Each(func(i int, selection *goquery.Selection) {
+			text := selection.Text()
+
+			if b := folk(c, text); b {
+				return
+			}
+
+			if f != nil {
+				if b := f(c, text); b {
+					f = nil
+				}
+			}
+
+			f = characterAssignFunc(sMap, text)
+		})
+
+	})
+	log.Println(*c)
 
 }
 
--- types.go
+++ types.go
@@ -26,11 +26,14 @@ type Character struct {
 	Pinyin         string //拼音
 	Radical        string //部首
 	RadicalStrokes string //部首笔画
+	TotalStrokes   string //总笔画
 	KangxiStrokes  string //康熙笔画数
 	Phonetic       string //注音
 	Folk
+	Structure
 }
 
+//民俗参考
 type Folk struct {
 	CommonlyCharacters   string //是否为常用字
 	NameScience          string //姓名学
@@ -38,16 +41,33 @@ type Folk struct {
 	GodBadMoral          string //吉凶寓意
 }
 
+//字形结构
 type Structure struct {
+	DecompositionSearch string //首尾分解查字
+	StrokeNumber        string //笔顺编号
+	StrokeReadWrite     string //笔顺读写
 }
 
+//音韵参考
 type Explain struct {
+
 }
 
+//索引参考
 type Rhyme struct {
+	GuangYun  string //广　韵
+	Mandarin  string //国　语
+	Cantonese string //粤　语
+
 }
 
 type Index struct {
+	AncientWrite      string //古文字诂林
+	HometownTrain     string //故训彙纂
+	Explain           string //说文解字
+	KangxiDictionary  string //康熙字典
+	ChineseDictionary string //汉语字典
+	Cihai             string //辞　海  
 }
 
 //NewRoot create an root
