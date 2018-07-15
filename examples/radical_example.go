7f5945268936c8d93c2f08cbaed240ab4f845367
godcong <jumbycc@163.com>
2018-07-16 03:30:05 +0800

    add example

---
+++ examples/radical_example.go
@@ -0,0 +1,40 @@
+package examples
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/godcong/excavator"
+	"github.com/godcong/excavator/db"
+	"gopkg.in/mgo.v2/bson"
+)
+
+//ExampleRadical how to get the radical
+func ExampleRadical() {
+	var rcs []excavator.RootCharacter
+	err := db.DB("root").Find(bson.M{}).All(&rcs)
+	if err != nil {
+
+	}
+	root := excavator.NewRoot("http://tool.httpcn.com", "/KangXi/BuShou.html")
+
+	for idx := range rcs {
+		radical := root.Radical(&rcs[idx])
+		radical.SetRoot(root)
+		radical.SetBefore(func(rc *excavator.RadicalCharacter) error {
+			count, err := db.DB("radical").Find(rc).Count()
+			if err != nil || count != 0 {
+				log.Println(err, count)
+				return fmt.Errorf("%s", "data found")
+			}
+			err = db.DB("radical").Insert(rc)
+			if err != nil {
+				log.Println(err)
+			}
+			return nil
+		})
+		radical.IteratorFunc(func(rc *excavator.RadicalCharacter) error {
+			return nil
+		})
+	}
+}
