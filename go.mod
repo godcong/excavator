2e6404327639f5b0b28c2040673a3db73220099d
godcong <jumbycc@163.com>
2019-07-27 14:28:24 +0800

    update ex

--- go.mod
+++ go.mod
@@ -3,11 +3,18 @@ module github.com/godcong/excavator
 require (
 	github.com/PuerkitoBio/goquery v1.4.1
 	github.com/andybalholm/cascadia v1.0.0 // indirect
-	github.com/chzyer/go-fetcher v0.0.0-20161022161328-4556944a79c5
+	github.com/antchfx/htmlquery v1.0.0 // indirect
+	github.com/antchfx/xmlquery v1.0.0 // indirect
+	github.com/antchfx/xpath v1.0.0 // indirect
 	github.com/globalsign/mgo v0.0.0-20180615134936-113d3961e731
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
 	github.com/godcong/go-trait v0.0.0-20190528080809-9a857488365f
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/tebeka/selenium v0.9.4
+	github.com/temoto/robotstxt v1.1.1 // indirect
 	github.com/urfave/cli v1.20.0
-	golang.org/x/net v0.0.0-20180709044923-6f138e0f6071 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
---
+++ json.go
@@ -0,0 +1,156 @@
+package excavator
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+)
+
+// UnionMustNotBeNull ...
+const UnionMustNotBeNull = "union must not be null"
+
+// UnparsableNumber ...
+const UnparsableNumber = "unparsable number"
+
+// UnionDoesNotContainNumber ...
+const UnionDoesNotContainNumber = "union does not contain number"
+
+// DecoderShouldNotReturnFloat64 ...
+const DecoderShouldNotReturnFloat64 = "decoder should not return float64"
+
+// UnionDoesNotContainBool ...
+const UnionDoesNotContainBool = "union does not contain bool"
+
+// UnionDoesNotContainString ...
+const UnionDoesNotContainString = "union does not contain string"
+
+// UnionDoesNotContainNull ...
+const UnionDoesNotContainNull = "union does not contain null"
+
+// UnionDoesNotContainObject ...
+const UnionDoesNotContainObject = "union does not contain object"
+
+// UnionDoesNotContainArray ...
+const UnionDoesNotContainArray = "union does not contain array"
+
+// CannotHandleDelimiter ...
+const CannotHandleDelimiter = "cannot handle delimiter"
+
+// CannotUnmarshalUnion ...
+const CannotUnmarshalUnion = "cannot unmarshal union"
+
+// marshalUnion ...
+func marshalUnion(pi *int64, pf *float64, pb *bool, ps *string, haveArray bool, pa interface{}, haveObject bool, pc interface{}, haveMap bool, pm interface{}, haveEnum bool, pe interface{}, nullable bool) ([]byte, error) {
+	if pi != nil {
+		return json.Marshal(*pi)
+	}
+	if pf != nil {
+		return json.Marshal(*pf)
+	}
+	if pb != nil {
+		return json.Marshal(*pb)
+	}
+	if ps != nil {
+		return json.Marshal(*ps)
+	}
+	if haveArray {
+		return json.Marshal(pa)
+	}
+	if haveObject {
+		return json.Marshal(pc)
+	}
+	if haveMap {
+		return json.Marshal(pm)
+	}
+	if haveEnum {
+		return json.Marshal(pe)
+	}
+	if nullable {
+		return json.Marshal(nil)
+	}
+	return nil, errors.New(UnionMustNotBeNull)
+}
+
+// unmarshalUnion ...
+func unmarshalUnion(data []byte, pi **int64, pf **float64, pb **bool, ps **string, haveArray bool, pa interface{}, haveObject bool, pc interface{}, haveMap bool, pm interface{}, haveEnum bool, pe interface{}, nullable bool) (bool, error) {
+	if pi != nil {
+		*pi = nil
+	}
+	if pf != nil {
+		*pf = nil
+	}
+	if pb != nil {
+		*pb = nil
+	}
+	if ps != nil {
+		*ps = nil
+	}
+
+	dec := json.NewDecoder(bytes.NewReader(data))
+	dec.UseNumber()
+	tok, err := dec.Token()
+	if err != nil {
+		return false, err
+	}
+
+	switch v := tok.(type) {
+	case json.Number:
+		if pi != nil {
+			i, err := v.Int64()
+			if err == nil {
+				*pi = &i
+				return false, nil
+			}
+		}
+		if pf != nil {
+			f, err := v.Float64()
+			if err == nil {
+				*pf = &f
+				return false, nil
+			}
+			return false, errors.New(UnparsableNumber)
+		}
+		return false, errors.New(UnionDoesNotContainNumber)
+	case float64:
+		return false, errors.New(DecoderShouldNotReturnFloat64)
+	case bool:
+		if pb != nil {
+			*pb = &v
+			return false, nil
+		}
+		return false, errors.New(UnionDoesNotContainBool)
+	case string:
+		if haveEnum {
+			return false, json.Unmarshal(data, pe)
+		}
+		if ps != nil {
+			*ps = &v
+			return false, nil
+		}
+		return false, errors.New(UnionDoesNotContainString)
+	case nil:
+		if nullable {
+			return false, nil
+		}
+		return false, errors.New(UnionDoesNotContainNull)
+	case json.Delim:
+		if v == '{' {
+			if haveObject {
+				return true, json.Unmarshal(data, pc)
+			}
+			if haveMap {
+				return false, json.Unmarshal(data, pm)
+			}
+			return false, errors.New(UnionDoesNotContainObject)
+		}
+		if v == '[' {
+			if haveArray {
+				return false, json.Unmarshal(data, pa)
+			}
+			return false, errors.New(UnionDoesNotContainArray)
+		}
+		return false, errors.New(CannotHandleDelimiter)
+	}
+	return false, errors.New(CannotUnmarshalUnion)
+
+}
