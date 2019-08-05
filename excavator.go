ada87f45522a1abc62358c97a1644f67a6d9762d
godcong <jumbycc@163.com>
2019-08-06 04:10:11 +0800

    update ex

--- excavator.go
+++ excavator.go
@@ -362,8 +362,8 @@ func (exc *Excavator) parseCharacter(characters <-chan *RadicalCharacter, char c
 				log.With("radical", cr.BuShou).Error(e)
 				continue
 			}
-
-			_, e = exc.db.InsertOne(ch)
+			e = ch.InsertIfNotExist(exc.db.NewSession())
+			//_, e = exc.db.InsertOne(ch)
 			if e != nil {
 				return
 			}
