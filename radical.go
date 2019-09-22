bfe9eb9284a225bf818c52ed81a15a6ebb6d2273
godcong <jumbycc@163.com>
2019-09-22 18:38:59 +0800

    char get test

--- radical.go
+++ radical.go
@@ -1,6 +1,11 @@
 package excavator
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gocolly/colly"
+	"strings"
+)
 
 // Radical ...
 type Radical []RadicalUnion
@@ -45,4 +50,54 @@ func (x *RadicalUnion) UnmarshalJSON(data []byte) error {
 // MarshalJSON ...
 func (x *RadicalUnion) MarshalJSON() ([]byte, error) {
 	return marshalUnion(nil, nil, nil, x.String, x.RadicalCharacterArray != nil, x.RadicalCharacterArray, false, nil, false, nil, false, nil, false)
+}
+
+func grabRadical(url string, characters chan<- *RadicalCharacter) {
+	if characters == nil {
+		return
+	}
+	defer func() {
+		characters <- nil
+	}()
+	c := colly.NewCollector()
+	c.OnHTML("a[href][data-action]", func(element *colly.HTMLElement) {
+		da := element.Attr("data-action")
+		log.With("value", da).Info("data action")
+		if da == "" {
+			return
+		}
+		q := NewQuery(url)
+
+		r, e := q.AJAX(strings.NewReader(fmt.Sprintf("wd=%s", da)))
+		if e != nil {
+			return
+		}
+		radical, e := UnmarshalRadical(r)
+		if e != nil {
+			return
+		}
+		for _, tmp := range *(*[]RadicalUnion)(radical) {
+			for i := range tmp.RadicalCharacterArray {
+				rc := tmp.RadicalCharacterArray[i]
+				//e := exc.saveRadicalCharacter(&tmp.RadicalCharacterArray[i])
+				//if e != nil {
+				//	log.Error(e)
+				//	continue
+				//}
+				characters <- &rc
+			}
+		}
+		log.With("value", r).Info("radical")
+	})
+	c.OnResponse(func(response *colly.Response) {
+		log.Info(string(response.Body))
+	})
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+	e := c.Visit(url)
+	if e != nil {
+		log.Error(e)
+	}
+	return
 }
