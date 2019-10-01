254d858620e93768de55e688d4d89dd363a35332
godcong <jumbycc@163.com>
2019-10-02 02:33:25 +0800

    update excavator

--- radical.go
+++ radical.go
@@ -79,16 +79,33 @@ func (x *RadicalUnion) MarshalJSON() ([]byte, error) {
 }
 
 func insertOrUpdateRadicalCharacter(engine *xorm.Engine, character *RadicalCharacter) (i int64, e error) {
-	i, e = engine.Where("hash = ?", net.Hash(character.URL)).Count(&RadicalCharacter{})
+	tmp := &RadicalCharacter{}
+	b, e := engine.Where("hash = ?", net.Hash(character.URL)).Get(tmp)
 	if e != nil {
-		return i, e
+		return 0, e
 	}
 
 	log.With("url", character.URL, "character", character.BuShou, "zi", character.Zi, "hash", net.Hash(character.URL)).Info("insert")
-	if i == 0 {
+	if !b {
 		return engine.InsertOne(character)
-	} else if i == 1 {
-		return engine.Where("hash = ?", net.Hash(character.URL)).Update(character)
 	}
-	return
+	copyRadicalCharacter(tmp, character)
+	return engine.Where("hash = ?", net.Hash(character.URL)).Update(tmp)
+}
+
+func stringCompareCopy(tg *string, src string) string {
+	if src != "" {
+		*tg = src
+	}
+	return *tg
+}
+
+func copyRadicalCharacter(tg, src *RadicalCharacter) {
+	stringCompareCopy(&tg.URL, src.URL)
+	stringCompareCopy(&tg.Alphabet, src.Alphabet)
+	stringCompareCopy(&tg.PinYin, src.PinYin)
+	stringCompareCopy(&tg.BuShou, src.BuShou)
+	stringCompareCopy(&tg.BiHua, src.BiHua)
+	stringCompareCopy(&tg.Zi, src.Zi)
+	stringCompareCopy(&tg.Num, src.Num)
 }
