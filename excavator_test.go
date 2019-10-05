85dcc65b4ccf1324e9a7cbbf4bddb06c5535e8f2
godcong <jumbycc@163.com>
2019-10-06 00:07:04 +0800

    update excavator

--- excavator_test.go
+++ excavator_test.go
@@ -3,10 +3,14 @@ package excavator
 import "testing"
 
 func TestExcavator_Run(t *testing.T) {
-	exc := New(RadicalTypeKangXiPinyin, ActionArgs(RadicalTypeKangXiPinyin, RadicalTypeKangXiBihua, RadicalTypeKangXiBushou))
-	e := exc.Run()
-	if e != nil {
-		return
+	excK := New(RadicalTypeKangXiPinyin, ActionArgs(RadicalTypeKangXiPinyin, RadicalTypeKangXiBihua, RadicalTypeKangXiBushou))
+	e1 := excK.Run()
+	if e1 != nil {
+		t.Fatal(e1)
 	}
-
+	excH := New(RadicalTypeHanChengPinyin, ActionArgs(RadicalTypeHanChengPinyin, RadicalTypeHanChengBihua, RadicalTypeHanChengBushou))
+	e2 := excH.Run()
+	if e2 != nil {
+		t.Fatal(e2)
+	}
 }
